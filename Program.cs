30343bca985147f08936d13bd2fdf9de6c306eb8
Sheeyx <sheeyx00@gmail.com>
2023-11-28 09:30:02 +0900

    project configuration

---
+++ Program.cs
@@ -0,0 +1,22 @@
+//=========================
+//Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace 
+//=========================
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Tarteeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
